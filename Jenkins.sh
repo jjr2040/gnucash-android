e91646efd1ea0920dbaf63dfde808af2bbf370e9
Juan Sebastian Mendez <juansebastian.mendezr@gmail.com>
2020-04-22 18:54:33 -0500

    Jenkins file

--- Jenkins.sh
+++ Jenkins.sh
@@ -16,11 +16,12 @@ if [ ! ${E2E_BDT} = "false" ] ; then
 	chmod +x scripts/run_android_features
 	cd scripts && ./run_android_features -r -d ${ANDROID_AVD_DEVICE}
 	cd ../../..
-	echo "------- FINISH BDT (CALABASH/CUCUMBER)"
+	echo "------- END BDT (CALABASH/CUCUMBER)"
 fi
 
 if [ ! ${RANDOM} = "false" ] ; then
 	echo "------- START MONKEY"
+	echo "${PWD}"
 	$ANDROID_HOME/platform-tools/adb install -r ${APK_PATH}/GnucashAndroid.apk
     $ANDROID_HOME/platform-tools/adb shell am start -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -n "org.gnucash.android.devel/org.gnucash.android.ui.account.AccountsActivity"
 	$ANDROID_HOME/platform-tools/adb shell monkey -p org.gnucash.android.devel -s ${RANDOM_SEED} -v ${RANDOM_EVENTS} >> tests/Monkey/monkey_results.txt
