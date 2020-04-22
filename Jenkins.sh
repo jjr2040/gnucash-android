2d2a0de665a1cbc09832d465e931520c62363c2b
Juan Sebastian Mendez <juansebastian.mendezr@gmail.com>
2020-04-22 18:40:29 -0500

    Added seed to the monkey

--- Jenkins.sh
+++ Jenkins.sh
@@ -3,6 +3,7 @@ ANDROID_AVD_DEVICE=$1
 E2E_BDT=$2
 RANDOM=$3
 RANDOM_EVENTS=$4
+RANDOM_SEED=$5
 
 
 mv -f ${APK_PATH}/*.apk ${APK_PATH}/GnucashAndroid.apk
@@ -19,5 +20,5 @@ fi
 if [ ! ${RANDOM} = "false" ] ; then
 	$ANDROID_HOME/platform-tools/adb install -r ${APK_PATH}/GnucashAndroid.apk
     $ANDROID_HOME/platform-tools/adb shell am start -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -n "org.gnucash.android.devel/org.gnucash.android.ui.account.AccountsActivity"
-	$ANDROID_HOME/platform-tools/adb shell monkey -p org.gnucash.android.devel -v ${RANDOM_EVENTS}
+	$ANDROID_HOME/platform-tools/adb shell monkey -p org.gnucash.android.devel -s ${RANDOM_SEED} -v ${RANDOM_EVENTS}
 fi
