dcd27784864e8c2242fbbc86b4fc5907bfce27f8
Juan Sebastian Mendez <juansebastian.mendezr@gmail.com>
2020-04-22 19:13:54 -0500

    Jenkins file

--- Jenkins.sh
+++ Jenkins.sh
@@ -23,6 +23,7 @@ if [ ! ${RANDOM} = "false" ] ; then
 	echo "------- START MONKEY"
 	$ANDROID_HOME/platform-tools/adb install -r ${APK_PATH}/GnucashAndroid.apk
     $ANDROID_HOME/platform-tools/adb shell am start -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -n "org.gnucash.android.devel/org.gnucash.android.ui.account.AccountsActivity"
+	echo "${PWD}"
 	$ANDROID_HOME/platform-tools/adb shell monkey -p org.gnucash.android.devel -s ${RANDOM_SEED} -v ${RANDOM_EVENTS} >> tests/Monkey/monkey_results.txt
 	echo "------- END MONKEY"
 fi
